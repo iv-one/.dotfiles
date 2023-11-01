05cacd79e0cf496ab434818ca85c0e24f78af377
Ivan Diachenko <iv.d@me.com>
2023-11-01 21:39:28 +0100

    chore: update unix/nix installation

--- nix/install-unix.sh
+++ nix/install-unix.sh
@@ -10,17 +10,14 @@ zsh
 
 curl https://nixos.org/nix/install | sh
 
-. /home/jocker/.nix-profile/etc/profile.d/nix.sh
-
 nix-env -i ripgrep
 
 nix-env -iA nixpkgs.jq
 nix-env -iA nixpkgs.neovim
-nix-env -iA nixpkgs.python35Packages.neovim
 nix-env -iA nixpkgs.platinum-searcher
 nix-env -iA nixpkgs.ag
-
 nix-env -iA nixpkgs.glibcLocales
+
 export LOCALE_ARCHIVE="$(readlink ~/.nix-profile/lib/locale)/locale-archive"
 
-chsh -s $(which zsh)
+# chsh -s $(which zsh)
---
+++ nix/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get -y install \
+curl \
+sudo \
+unzip \
+xz-utils \
+wget \
+zsh
+
+curl https://nixos.org/nix/install | sh
+
+nix-env -i ripgrep
+
+nix-env -iA nixpkgs.jq
+nix-env -iA nixpkgs.neovim
+nix-env -iA nixpkgs.platinum-searcher
+nix-env -iA nixpkgs.ag
+nix-env -iA nixpkgs.glibcLocales
+
+export LOCALE_ARCHIVE="$(readlink ~/.nix-profile/lib/locale)/locale-archive"
+
+# chsh -s $(which zsh)
