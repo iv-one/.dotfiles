d87fa122e154d71d6a23bc0ba326004813501f8c
Ivan Diachenko <iv.d@me.com>
2023-11-01 21:39:13 +0100

    Fix OS detection for tmux installation

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,8 +4,8 @@ dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 unameout="$(uname -s)"
 case "${unameout}" in
-  linux*)     sed -i 's|color_main="colour[0-9]*"|color_main="colour214"|g' "$dirname/tmux.conf";;
-  darwin*)    sed -i '' 's|color_main="colour[0-9]*"|color_main="colour39"|g' "$dirname/tmux.conf";;
+  Linux*)     sed -i 's|color_main="colour[0-9]*"|color_main="colour214"|g' "$dirname/tmux.conf";;
+  Darwin*)    sed -i '' 's|color_main="colour[0-9]*"|color_main="colour39"|g' "$dirname/tmux.conf";;
 esac
 
 src="$dirname/tmux.conf"
