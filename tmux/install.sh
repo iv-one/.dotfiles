bedd1364263d09314176bebaa2a4497dab087eb1
Ivan Diachenko <iv.d@me.com>
2023-11-01 21:39:12 +0100

    Tmux use different colors for Linux/Mac

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,6 +2,15 @@
 
 dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+  Linux*)     color="colour214";;
+  Darwin*)    color="colour39";;
+  *)          color="colour202";;
+esac
+
+sed -i '' "s|color_main=\"colour[0-9]*\"|color_main=\"$color\"|g" "$dirname/tmux.conf"
+
 src="$dirname/tmux.conf"
 dist="$HOME/.tmux.conf"
 
