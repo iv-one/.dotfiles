1db6a0375dda9c5d4534e1e6bb68dd6ac4e8fdf9
Ivan Diachenko <iv.d@me.com>
2023-11-01 21:39:24 +0100

    Install git and tools for Linux/Max

--- git/install.sh
+++ git/install.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+function install_linux_git {
+  sudo apt-get install git -y
+  
+  nix-env -i tig
+  nix-env -i diff-so-fancy
+  
+  configure_git
+}
 
-src="$dirname/gitconfig"
-dist="$HOME/.gitconfig"
+function install_darwin_git {
+  brew install git tig diff-so-fancy
+  
+  configure_git
+}
 
-rm -f $dist
-ln -s $src $dist
+function configure_git() {
+  dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  
+  src="$dirname/gitconfig"
+  dist="$HOME/.gitconfig"
+  
+  rm -f $dist
+  ln -s $src $dist
+}
+
+unameout="$(uname -s)"
+case "${unameout}" in
+  Linux*)     install_linux_git;;
+  Darwin*)    install_darwin_git;;
+esac
