d0b8f7dff624a3717432a360ba5ce18194019123
Ivan Diachenko <iv.d@me.com>
2023-11-01 21:39:15 +0100

    Install The Silver Searcher

--- unix/install.sh
+++ unix/install.sh
@@ -8,6 +8,7 @@ nix-env -iA nixpkgs.jq
 nix-env -iA nixpkgs.neovim
 nix-env -iA nixpkgs.python35Packages.neovim
 nix-env -iA nixpkgs.platinum-searcher
+nix-env -iA nixpkgs.ag
 
 nix-env -iA nixpkgs.glibcLocales
 export LOCALE_ARCHIVE="$(readlink ~/.nix-profile/lib/locale)/locale-archive"
