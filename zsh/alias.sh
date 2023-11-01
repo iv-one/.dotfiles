401a7346559ea3075f3ee1d7fc01b8f42a1dacd0
Ivan Diachenko <iv.d@me.com>
2023-11-01 21:39:29 +0100

    feat: imporve theme command

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,2 +1,2 @@
-alias theme-dark="cp -f ~/.dotfiles/alacritty/alacritty-nord.yml ~/.dotfiles/alacritty/alacritty.yml"
-alias theme-light="cp -f ~/.dotfiles/alacritty/alacritty-light.yml ~/.dotfiles/alacritty/alacritty.yml"
+alias theme-dark="cp -f ~/.dotfiles/alacritty/alacritty-nord.yml ~/.dotfiles/alacritty/alacritty.yml && cp -f ~/.dotfiles/git/gitconfig-dark ~/.dotfiles/git/gitconfig"
+alias theme-light="cp -f ~/.dotfiles/alacritty/alacritty-light.yml ~/.dotfiles/alacritty/alacritty.yml && cp -f ~/.dotfiles/git/gitconfig-light ~/.dotfiles/git/gitconfig"
