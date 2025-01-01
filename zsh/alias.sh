044dfaec9fac18fb073a707a6dba1350071b6616
Ivan Diachenko <iv.d@me.com>
2025-01-01 11:03:43 +0100

    chore: add k for kubectl alias

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,2 +1,3 @@
 alias theme-dark="cp -f ~/.dotfiles/alacritty/alacritty-nord.yml ~/.dotfiles/alacritty/alacritty.yml && cp -f ~/.dotfiles/git/gitconfig-dark ~/.dotfiles/git/gitconfig"
 alias theme-light="cp -f ~/.dotfiles/alacritty/alacritty-light.yml ~/.dotfiles/alacritty/alacritty.yml && cp -f ~/.dotfiles/git/gitconfig-light ~/.dotfiles/git/gitconfig"
+alias k="kubectl"
